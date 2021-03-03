6cc708969ca9834b497a2aa037b3d1c35dcd35f3
Boomski99 <krisnadwipayana30@gmail.com>
2021-03-03 14:14:28 +0800

    Simple Update

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,10 +1,10 @@
 import React from "react";
-// react plugin for creating charts
+
 import ChartistGraph from "react-chartist";
-// @material-ui/core
+
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
+
 import Store from "@material-ui/icons/Store";
 import Warning from "@material-ui/icons/Warning";
 import DateRange from "@material-ui/icons/DateRange";
@@ -16,7 +16,7 @@ import Accessibility from "@material-ui/icons/Accessibility";
 import BugReport from "@material-ui/icons/BugReport";
 import Code from "@material-ui/icons/Code";
 import Cloud from "@material-ui/icons/Cloud";
-// core components
+
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import Table from "components/Table/Table.js";
@@ -63,7 +63,7 @@ export default function Dashboard() {
                   <Warning />
                 </Danger>
                 <a href="#pablo" onClick={e => e.preventDefault()}>
-                  Get more space
+                 Info Lanjutan
                 </a>
               </div>
             </CardFooter>
@@ -81,7 +81,7 @@ export default function Dashboard() {
             <CardFooter stats>
               <div className={classes.stats}>
                 <DateRange />
-                Last 24 Hours
+                Info Lanjutan
               </div>
             </CardFooter>
           </Card>
@@ -98,7 +98,7 @@ export default function Dashboard() {
             <CardFooter stats>
               <div className={classes.stats}>
                 <LocalOffer />
-                Tracked from Github
+                Info Lanjutan
               </div>
             </CardFooter>
           </Card>
